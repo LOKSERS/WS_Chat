77d56f43856b0f7d02455b4e16343483240e751c
Dmitrii Ganichev <Dmtr.Lks@yandex.ru>
2021-09-17 15:35:26 +0300

    first commit

---
+++ src/test/java/com/example/websocketapp/WebsocketappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websocketapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
